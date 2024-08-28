4890c57a59f1b15494c271c34be80a26c91663ed
Augusto Gomes <augusto.santos@ccc.ufcg.edu.br>
2024-08-28 17:47:10 -0300

    Setup PostgreSQL

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,6 @@
-import express, { Request, Response } from 'express';
-import dotenv from 'dotenv';
+import app from './app';
 
-dotenv.config();
-
-const app = express();
 const port = process.env.PORT || 3000;
-
-app.use(express.json());
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
-});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
